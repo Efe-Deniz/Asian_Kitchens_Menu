e46fe779bf0c255fa6093adfcf898fab80bc33fb
Ali Efe Deniz <dnz.ali.efe@gmail.com>
2026-02-05 12:37:45 +0300

    Create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+//Bu componentin görevi sayfanın üstünde başlığı göstermek
+export const Header = () => {
+    return (
+        //jsx döndür
+        <header className="text-center py-12 bg-white">
+            <h1 className="text-5xl font-bold text-orange-600 mb-2">Asian Kitchen's Menu</h1>
+        </header>
+    );
+};
